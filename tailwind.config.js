1b7420a93e592f1ce912406c63f1600d1f9eacfd
nima <nimashahbazi524@gmail.com>
2021-06-13 16:53:45 +0430

    Init commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: [
+    './**/*.vue'
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
